45cb5a4b07c828210006a9f318a0bfa6f8099ace
KrishnaKumar99 <52557268+KrishnaKumar99@users.noreply.github.com>
2019-07-06 20:35:02 +0530

    Create vow.py

---
+++ vow.py
@@ -0,0 +1,7 @@
+ltr=input()
+if(ltr=='a' or ltr=='A' or ltr=='e' or ltr=='E' or ltr=='i' or ltr=='I' or ltr=='o' or ltr=='O' or ltr=='u' or ltr=='U'):
+    print("Vowel")
+elif(ltr=='@' or ltr=='#' or ltr=='$' or ltr=='%' or ltr=='^' or ltr=='&' or ltr=='*'):
+    print("invalid")
+else:
+    print("Consonant")
